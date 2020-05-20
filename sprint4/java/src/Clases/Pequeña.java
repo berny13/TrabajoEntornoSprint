2728adfdcd83910712b6e60669a2f638e0bc8d49
joserivera93 <47832779+joserivera93@users.noreply.github.com>
2020-05-20 15:21:53 +0200

    Subo la carpeta de los archivos java

---
+++ sprint4/java/src/Clases/Pequeña.java
@@ -0,0 +1,37 @@
+package Clases;
+
+public class Pequeña {
+
+	String sabor;
+	int cantidad;
+	
+	public Pequeña() {}
+
+	public Pequeña(String sabor, int cantidad) {
+		
+		this.sabor = sabor;
+		this.cantidad = cantidad;
+	}
+
+	public String getSabor() {
+		return sabor;
+	}
+
+	public void setTipo(String sabor) {
+		this.sabor = sabor;
+	}
+
+	public void getCantidad() {
+		return Cantidad;
+	}
+
+	public void setcantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public boolean comprar(String Bebida) {
+		return true;
+	}
+	public boolean registrar (String Bebida) {
+		return true;
+	}
+}
---
+++ sprint4/java/src/Clases/Sin_Alcohol.java
@@ -0,0 +1,39 @@
+package Clases;
+
+public class Sin_Alcohol{
+
+	String marca;
+	String tipo;
+	
+	public Sin_Alcohol() {}
+
+	public Sin_Alcohol(String marca, String tipo) {
+		
+		this.marca = marca;
+		this.tipo = tipo;
+	}
+
+	public String getMarca() {
+		return this.marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public double getTipo() {
+		return this.GradoAlcohol;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public void comprar(String Bebida) {
+		
+	}
+	public void registrar(String Bebida) {
+		
+	}
+}
+
+
